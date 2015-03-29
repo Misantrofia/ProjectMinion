e651d4843b8e84962505d5fa8a365eefa1c4b09f
Alexandru schwartz <alexandru.schwartz@softvision.ro>
2015-03-29 21:43:25 +0300

    Initial setup of the project

---
+++ ProjectMinion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectMinion
+//
+//  Created by Alexandru schwartz on 29/03/15.
+//  Copyright (c) 2015 Alexandru schwartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
